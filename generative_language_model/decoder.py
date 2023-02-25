d6baf9b0bcfbeb4b5e915fce987b1eac949562ab
JohnnyBui <johnny.bui.jb@gmail.com>
2023-02-24 23:39:22 -0800

    [generative_language_model] implemented beam search

--- generative_language_model/decoder.py
+++ generative_language_model/decoder.py
@@ -52,50 +52,54 @@ def main():
     seed = 42
     mlen = 150
 
-    torch.manual_seed(seed); np.random.seed(seed)
-    print("\n----------- Vanilla Sampling -----------")
-    print(sample(lm, text_field, prompt=p, max_len=mlen))
+    # torch.manual_seed(seed); np.random.seed(seed)
+    # print("\n----------- Vanilla Sampling -----------")
+    # print(sample(lm, text_field, prompt=p, max_len=mlen))
     
-    torch.manual_seed(seed); np.random.seed(seed)
-    print("\n------- Temp-Scaled Sampling 0.0001 -------")
-    print(sample(lm, text_field, prompt=p, temp=0.0001, max_len=mlen))
+    # torch.manual_seed(seed); np.random.seed(seed)
+    # print("\n------- Temp-Scaled Sampling 0.0001 -------")
+    # print(sample(lm, text_field, prompt=p, temp=0.0001, max_len=mlen))
     
-    torch.manual_seed(seed); np.random.seed(seed)
-    print("\n------- Temp-Scaled Sampling 100 --------")
-    print(sample(lm, text_field, prompt=p, temp=100, max_len=mlen))
+    # torch.manual_seed(seed); np.random.seed(seed)
+    # print("\n------- Temp-Scaled Sampling 100 --------")
+    # print(sample(lm, text_field, prompt=p, temp=100, max_len=mlen))
     
-    torch.manual_seed(seed); np.random.seed(seed)
-    print("\n----------- Top-k Sampling 1 -----------")
-    print(sample(lm, text_field, prompt=p, k=1, max_len=mlen))
+    # torch.manual_seed(seed); np.random.seed(seed)
+    # print("\n----------- Top-k Sampling 1 -----------")
+    # print(sample(lm, text_field, prompt=p, k=1, max_len=mlen))
     
-    torch.manual_seed(seed); np.random.seed(seed)
-    print("\n----------- Top-k Sampling 20 -----------")
-    print(sample(lm, text_field, prompt=p, k=20, max_len=mlen))
+    # torch.manual_seed(seed); np.random.seed(seed)
+    # print("\n----------- Top-k Sampling 20 -----------")
+    # print(sample(lm, text_field, prompt=p, k=20, max_len=mlen))
     
-    torch.manual_seed(seed); np.random.seed(seed)
-    print("\n----------- Top-p Sampling 0.001 -----------")
-    print(sample(lm, text_field, prompt=p, p=0.001, max_len=mlen))
+    # torch.manual_seed(seed); np.random.seed(seed)
+    # print("\n----------- Top-p Sampling 0.001 -----------")
+    # print(sample(lm, text_field, prompt=p, p=0.001, max_len=mlen))
 
-    torch.manual_seed(seed); np.random.seed(seed)
-    print("\n----------- Top-p Sampling 0.75 -----------")
-    print(sample(lm, text_field, prompt=p, p=0.75, max_len=mlen))
+    # torch.manual_seed(seed); np.random.seed(seed)
+    # print("\n----------- Top-p Sampling 0.75 -----------")
+    # print(sample(lm, text_field, prompt=p, p=0.75, max_len=mlen))
 
-    torch.manual_seed(seed); np.random.seed(seed)
-    print("\n----------- Top-p Sampling 1 -----------")
-    print(sample(lm, text_field, prompt=p, p=1, max_len=mlen))
+    # torch.manual_seed(seed); np.random.seed(seed)
+    # print("\n----------- Top-p Sampling 1 -----------")
+    # print(sample(lm, text_field, prompt=p, p=1, max_len=mlen))
     
-
     torch.manual_seed(seed); np.random.seed(seed)
     print("\n----------- Beam Search B=1 -----------")
-    print(beamsearch(lm, text_field, prompt=p, beams=1, max_len=mlen))
+    print(beamsearch(lm, text_field, prompt=p, beams=2, max_len=mlen))
+    
+    
+    # torch.manual_seed(seed); np.random.seed(seed)
+    # print("\n----------- Beam Search B=1 -----------")
+    # print(beamsearch(lm, text_field, prompt=p, beams=1, max_len=mlen))
 
-    torch.manual_seed(seed); np.random.seed(seed)
-    print("\n----------- Beam Search B=10 -----------")
-    print(beamsearch(lm, text_field, prompt=p, beams=10, max_len=mlen))
+    # torch.manual_seed(seed); np.random.seed(seed)
+    # print("\n----------- Beam Search B=10 -----------")
+    # print(beamsearch(lm, text_field, prompt=p, beams=10, max_len=mlen))
 
-    torch.manual_seed(seed); np.random.seed(seed)
-    print("\n----------- Beam Search B=50 -----------")
-    print(beamsearch(lm, text_field, prompt=p, beams=50, max_len=mlen))
+    # torch.manual_seed(seed); np.random.seed(seed)
+    # print("\n----------- Beam Search B=50 -----------")
+    # print(beamsearch(lm, text_field, prompt=p, beams=50, max_len=mlen))
 
     print()
 
@@ -106,8 +110,132 @@ def main():
 ############################################################################################
 
 def beamsearch(model, text_field, beams=5, prompt="", max_len=50):
-    decodedString = "Not implemented"
-    return decodedString
+    model.to(dev)
+    model.eval()
+    
+    with torch.no_grad():
+        prompt_tokens = text_field.process([text_field.tokenize(prompt.lower())]).to(dev)
+
+        # Init
+        h = torch.zeros([beams, model.rnn.num_layers, 1, model.rnn.hidden_size]).to(dev)
+        c = torch.zeros([beams, model.rnn.num_layers, 1, model.rnn.hidden_size]).to(dev)
+        all_top_word_values = torch.zeros([beams, beams])
+        all_top_word_indices = torch.zeros([beams, beams], dtype=int)
+        
+        # Process the prefix
+        out, h, c = model(prompt_tokens, h[0], c[0])
+        
+        log_probs = torch.log(softmax(torch.squeeze(out[-1]), 0))
+        combine_top_words = torch.topk(log_probs, beams)
+        # all_log_probs = combine_top_words.values.repeat([beams,1])
+        
+        h = h.repeat([beams, 1, 1, 1])
+        c = c.repeat([beams, 1, 1, 1])
+        
+        # w = torch.zeros([beams]).to(dev)
+        # w = prompt_tokens[-1].repeat([beams, 1])
+        w = combine_top_words.indices
+        
+        # text = prompt_tokens.repeat([beams,1])
+        text = prompt_tokens.view([1,-1]).repeat([beams,1])
+        text = text.type(torch.IntTensor)
+
+        sample_len = max_len - len(prompt_tokens)
+        
+        # Init k prev log prob as 0 because log(1) = 0.
+        # prev_log_prob = torch.zeros([beams])
+        prev_log_prob = combine_top_words.values
+        
+        for _ in range(0, sample_len):
+            # all_log_probs = torch.zeros([beams, text_field.vocab.__len__()])
+            
+            for i in range(0, beams):
+                # out, h[i], c[i] = model(w[i], h[i], c[i])
+                out, h[i], c[i] = model(w[i].view([1,-1]), h[i], c[i])
+                
+                # accumulate log prob (add)
+                log_probs = torch.log(softmax(torch.squeeze(out), 0))
+                top_words_per_beam = torch.topk(log_probs, beams)
+                # log_probs += prev_log_prob[i]
+                
+                # combine Vxk, then find topk log prob
+                # top_log_probs = torch.topk(log_probs, beams)
+                
+                # all_log_probs = torch.cat(all_log_probs, log_probs, 0)
+                # all_log_probs = torch.cat((all_log_probs, log_probs), 1)
+                # all_log_probs[i] = log_probs
+                # all_log_probs[i] = top_words_per_beam.values
+                all_top_word_values[i] = prev_log_prob[i] + top_words_per_beam.values
+                all_top_word_indices[i] = top_words_per_beam.indices
+                
+                # all_top_word_values = torch.cat([all_top_word_values, top_words_per_beam.values])
+                # all_top_word_indices = torch.cat([all_top_word_indices, top_words_per_beam.indices])
+                
+            # Pick the top-k log prob
+            # top_k = torch.topk(all_log_probs, beams)
+            flatten_all_top_word_values = all_top_word_values.view([-1])
+            flatten_all_top_word_indices = all_top_word_indices.view([-1])
+            
+            top_words = torch.topk(flatten_all_top_word_values, beams)
+            
+            prev_log_prob = top_words.values
+            
+            history_indices = top_words.indices / beams
+            
+            # new_text = torch.Tensor([])
+            # new_text = torch.cat((text, torch.zeros([beams, 1], dtype=int)), 1)
+            
+            new_text = text[history_indices.type(torch.LongTensor)]
+            # text[history_indices.type(torch.LongTensor)]
+            new_words = flatten_all_top_word_indices[top_words.indices]
+            new_text = torch.cat((new_text, new_words.view([-1,1])), 1)
+            
+            # for i in range(0, beams):
+            #     # history = text[int(history_indices[i])]
+            #     text[i][-1] = int(flatten_all_top_word_indices[top_words.indices[i]].view([1]))
+                
+                # text[i][-1] = torch.cat((history, new_word), 0)
+                
+                # if i == 0:
+                #     text[i][-1] = torch.cat((history, new_word), 0)
+                # else:
+                #     new_history = torch.cat((history, new_word), 0)
+                #     new_text = torch.cat((new_text.view([1,-1]), new_history.view([1,-1])), 0) 
+                
+            text = new_text
+            
+            # For debug. TODO: run it in debug mode and check when it goes wrong
+            _, best_text_idx = torch.topk(top_words.values, 1)
+            print(text[:,-10:])
+            print(reverseNumeralize(text[top_words.indices[int(best_text_idx)]], text_field))
+            
+            
+            # dist = torch.squeeze(softmax(out, 1))
+            
+            # if k != 0:
+            #     top_k = torch.topk(dist, k)
+            #     sum_top_k = torch.sum(top_k.values)
+            #     dist = torch.zeros_like(dist)
+            #     dist[top_k.indices] = top_k.values
+            #     dist = torch.div(dist, sum_top_k)
+            # elif p != 1:
+            #     sorted_dist = torch.sort(dist, descending=True)
+            #     prefix_sum = torch.cumsum(sorted_dist.values, 0)
+            #     cut_off_idx = (prefix_sum >= p).nonzero(as_tuple=True)[0][0]
+                
+            #     # Zero out words not in the min-p set
+            #     dist[sorted_dist.indices[cut_off_idx+1 :]] = 0
+                
+            #     sum_top_p = prefix_sum[cut_off_idx]
+                
+            #     dist = torch.div(dist, sum_top_p)
+            
+            # w = torch.distributions.Categorical(dist).sample().resize(1,1)
+            # prompt_tokens = torch.cat((prompt_tokens, w), 0)
+            
+        _, best_text_idx = torch.topk(top_words.values, 1)
+        
+    return reverseNumeralize(text[top_words.indices[int(best_text_idx)]], text_field)
 
 ############################################################################################
 # TASK 1.2
@@ -115,10 +243,6 @@ def beamsearch(model, text_field, beams=5, prompt="", max_len=50):
 
 def sample(model, text_field, prompt="", max_len=50, temp=1.0, k=0, p=1):
     assert (k==0 or p==1), "Cannot combine top-k and top-p sampling"
-    decodedString = "Not implemented"
-    
-    decode_string = prompt + " "
-        
     model.to(dev)
     model.eval()
     
@@ -131,9 +255,7 @@ def sample(model, text_field, prompt="", max_len=50, temp=1.0, k=0, p=1):
         h = torch.zeros([model.rnn.num_layers, 1, model.rnn.hidden_size]).to(dev)
         c = torch.zeros([model.rnn.num_layers, 1, model.rnn.hidden_size]).to(dev)
 
-        n_top = 1
         sample_len = max_len - len(prompt_tokens)
-        decode_tokens = torch.empty([sample_len,n_top], dtype=int)
         
         for _ in range(0, sample_len):
             out, h, c = model(w, h, c)
@@ -149,11 +271,9 @@ def sample(model, text_field, prompt="", max_len=50, temp=1.0, k=0, p=1):
             elif p != 1:
                 sorted_dist = torch.sort(dist, descending=True)
                 prefix_sum = torch.cumsum(sorted_dist.values, 0)
-                
                 cut_off_idx = (prefix_sum >= p).nonzero(as_tuple=True)[0][0]
                 
                 # Zero out words not in the min-p set
-                # sorted_dist.values[cut_off_idx + 1 :] = 0
                 dist[sorted_dist.indices[cut_off_idx+1 :]] = 0
                 
                 sum_top_p = prefix_sum[cut_off_idx]
@@ -161,9 +281,9 @@ def sample(model, text_field, prompt="", max_len=50, temp=1.0, k=0, p=1):
                 dist = torch.div(dist, sum_top_p)
             
             w = torch.distributions.Categorical(dist).sample().resize(1,1)
-            decode_tokens[_] = w
+            prompt_tokens = torch.cat((prompt_tokens, w), 0)
         
-    return prompt + " " + reverseNumeralize(decode_tokens, text_field)
+    return reverseNumeralize(prompt_tokens, text_field)
 
 ############################################################################################
 
